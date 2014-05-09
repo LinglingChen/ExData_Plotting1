4ac034faad00cb25f75e575dcb063e18c410ff70
lchen008@gmail.com <lxc96@R8D2Z2P.acs.org>
2014-05-09 12:57:36 -0600

    Course Project 1 updated

--- plot2.R
+++ plot2.R
@@ -1,10 +1,9 @@
 # Plot2 on Household power consumption
 
-source("plot1.R")
-date.time <- strptime(paste(datSub$Date, datSub$Time), format='%d/%m/%Y %H:%M:%S')
+dat<-read.table("household_power_consumption.txt", header=T, sep=";")
+datSub<-dat[dat$Date %in% c("1/2/2007","2/2/2007"),]
 datSub$Global_active_power<-as.numeric(as.character(datSub$Global_active_power))
-plot2<-function(x,y) {plot(x,y,type="l",xlab="",ylab="")}
+date.time <- strptime(paste(datSub$Date, datSub$Time), format='%d/%m/%Y %H:%M:%S')
 png(file="plot2.png",width=480,height=480)
-plot2(date.time,datSub$Global_active_power)
-title(ylab='Global Active Power (kilowatts)')
+plot(date.time,datSub$Global_active_power, type="l",xlab="",ylab="Global Active Power (kilowatts)")
 dev.off()
--- plot3.R
+++ plot3.R
@@ -1,13 +1,13 @@
 # Plot3 on Household power consumption
 
-source("plot2.R")
+dat<-read.table("household_power_consumption.txt", header=T, sep=";")
+datSub<-dat[dat$Date %in% c("1/2/2007","2/2/2007"),]
+date.time <- strptime(paste(datSub$Date, datSub$Time), format='%d/%m/%Y %H:%M:%S')
 datSub$Sub_metering_1<-as.numeric(as.character(datSub$Sub_metering_1))
 datSub$Sub_metering_2<-as.numeric(as.character(datSub$Sub_metering_2))
-datSub$Sub_metering_3<-as.numeric(as.character(datSub$Sub_metering_3))
-plot3<-function() {plot(date.time,datSub$Sub_metering_1,type="l",xlab="",ylab='Energy sub metering')
+png("plot3.png",width = 480, height = 480)
+plot(date.time,datSub$Sub_metering_1,type="l",xlab="",ylab='Energy sub metering')
 lines(date.time,datSub$Sub_metering_2,type="l",xlab="", col="red")
 lines(date.time, datSub$Sub_metering_3,col="blue")
-legend("topright",lty = 1, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))}
-png("plot3.png",width = 480, height = 480)
-plot3()
+legend("topright",lty = 1, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 dev.off()
